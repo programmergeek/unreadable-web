ad913451d242fc391067272437ae0342a97cc79e
Will <programergeek@gmail.com>
2022-04-08 18:42:42 +0200

    create function to send message to content script when the button is clicked

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,27 @@
 import React from "react";
 import "./App.css";
 
+const handleClick = () => {
+  chrome.tabs &&
+    chrome.tabs.query(
+      {
+        active: true,
+        currentWindow: true,
+      },
+      (tabs) => {
+        chrome.tabs.sendMessage(
+          tabs[0].id || 0,
+          "SCRAMBLE_TEXT",
+          (response: string[]) => console.log(response)
+        );
+      }
+    );
+};
+
 function App() {
   return (
     <div className="App">
-      <button>Make Me Illiterate</button>
+      <button onClick={() => handleClick()}>Make Me Illiterate</button>
     </div>
   );
 }
